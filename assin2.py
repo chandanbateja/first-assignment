75b08bafb5acc1c5a3d691e3b82f768fa8fc4300
chandanbateja <bateja43@gmail.com>
2018-07-02 21:09:49 +0530

    assin2.py

---
+++ assin2.py
@@ -0,0 +1,37 @@
+# Print anything on screen
+
+print ("Welcome to Python")
+
+
+#join using strings
+
+a='Acad'
+b='View'
+print(a+b)
+
+
+#input of three variables
+
+a=10
+b=30
+c=50
+print(a,b,c)
+
+
+#print on screen
+
+print("Let's get Started")
+
+
+#print using placeholders
+
+s="Acadview"
+course="python"
+fees=5000
+print('%s %s %d'%(s,course,fees))
+
+#exercise
+
+name="Tony Stark"
+salary=1000000
+print('%s %d'%(name,salary))
