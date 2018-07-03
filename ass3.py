2719957ff51456838006d8aecb98d1ed6243f301
chandanbateja <40684156+chandanbateja@users.noreply.github.com>
2018-07-03 18:33:32 +0530

    Add files via upload

---
+++ ass3.py
@@ -0,0 +1,39 @@
+#Question 1
+
+A=[input("enter the list")for i in range(5)]
+print(A)
+
+
+#Question 2
+
+B=['google','apple','facebook','microsoft','tesla']
+c=print(A+B)
+
+#Question 3
+
+B=['google','apple','facebook','microsoft','tesla']
+print (B)
+print (B.count('apple'))
+
+#question 4
+
+d=[2,5,8,1,9,4]
+d.sort()
+print(d)
+
+#Question 5
+
+a=[1,2,3,4]
+b=[7,5,8,3]
+c=a+b
+c.sort()
+print(c)
+
+#Question 6
+
+stack=["rithvik","shilpa","akshay"]
+print(stack)
+stack.append("chandan")
+print(stack)
+stack.pop()
+print(stack)
